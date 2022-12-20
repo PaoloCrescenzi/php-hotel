4b89b1b4e6faa24b7e31b5cdaf81d11fc37974d4
PaoloCrescenz <crescenzipaolo2@gmail.com>
2022-12-20 16:23:30 +0100

    Stampa dell'array su tabella

--- index.php
+++ index.php
@@ -48,9 +48,41 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css"
+    integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=="
+    crossorigin="anonymous" referrerpolicy="no-referrer" />
+  <title>php-hotels</title>
 </head>
 <body>
-  
+<div class="container mt-5">
+  <table class="table table-hover table-dark">
+    <thead>
+      <tr>
+        <th scope="col">Nome</th>
+        <th scope="col">Descrizione</th>
+        <th scope="col">Parcheggio</th>
+        <th scope="col">voto</th>
+        <th scope="col">DIstanza dal centro</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+      foreach ($hotels as $hotels) {
+      ?>
+        <tr>
+          <td><?php echo $hotels["name"] ?></td>
+          <td><?php echo $hotels["description"] ?></td>
+          <td><?php echo $hotels["parking"] ?></td>
+          <td><?php echo $hotels["vote"] ?></td>
+          <td><?php echo $hotels["distance_to_center"] ?> km</td>
+        </tr>
+      <?php
+      }
+      ?>
+    </tbody>
+  </table>
+</div>
+
 </body>
 </html>
